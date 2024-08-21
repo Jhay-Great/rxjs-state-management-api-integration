f551a209c1f4a499ff8c89127fd0018cca654ab5
Jhay-Great <alemonai.jenef@gmail.com>
2024-08-21 15:16:55 +0000

    adds initial folder structure and files

---
+++ src/app/components/shopping-items/shopping-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping-items.component.html',
+  styleUrl: './shopping-items.component.css'
+})
+export class ShoppingItemsComponent {
+
+}
