616e6a639893821669797fa5c89ab1857bbb30ce
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-06 03:51:31 +0000

    cleans up shopping-card component code base

--- src/app/components/shopping-card/shopping-card.component.ts
+++ src/app/components/shopping-card/shopping-card.component.ts
@@ -19,33 +19,17 @@ export class ShoppingCardComponent implements OnInit {
 
   @Input () product!: Product;
 
-  // cartProductIds: Set<string> = new Set<string>();
-  addToCartIsClicked:boolean = false;
-  selectedProductId: string | null = null;
-  clicked:string[] = [];
-  orderQuantity!:number;
   orderData!:Observable<OrderItem[]>;
+  addToCartIsClicked:boolean = false;
   productName!:string;
-
-  // special as well
-  cartState: Map<string, boolean> = new Map();
+  
 
   constructor (
-    private productService: ShopDataService,
     private cartService: CartService,
   ) {  }
   
   ngOnInit(): void {
     this.orderData = this.cartService.getCartItems();
-    
-    
-    
-    
-    // console.log(this.product)
-    // this.cartService.cartState$.subscribe((state:any) => {
-    //   this.cartState = state;
-    // });
-    
   }
 
   onAddToCart (id:string) { 
@@ -58,7 +42,6 @@ export class ShoppingCardComponent implements OnInit {
     this.cartService.decreaseQuantity(id);
     this.cartService.getQuantityCount(id).subscribe(
       quantity => {
-        // console.log(quantity);
         if (quantity > 0) return;
         this.cartService.removeOrderFromCart(id);
         this.addToCartIsClicked = false; // use this to display addToCart btn
@@ -71,71 +54,5 @@ export class ShoppingCardComponent implements OnInit {
   onIncreaseProductQuantity (id:string) {
     this.cartService.increaseQuantity(id)
   }
-
-
-  // addProductToCart (id:string) {
-  //   this.cartService.updateCartState(id, true);
-  //   // this.addToCartIsClicked = true;
-  //   const FIRST_ITEM_ADDED = 1;
-  //   this.orderQuantity = FIRST_ITEM_ADDED;
-  //   const data = this.productService.findItem(id).pipe(
-  //     map(data => ({
-  //       orderId: '3',
-  //       name: data?.name,
-  //       price: data?.price,
-  //       productId: data?.id, 
-  //       quantityCount: FIRST_ITEM_ADDED
-  //     }))
-  //   )
-  //   this.cartService.addToCart(data);
-
-
-  // }
   
-  // increaseProductQuantity(productId: string) {
-
-  //   this.cartService.findItem(productId).pipe(
-  //     take(1),
-  //     filter((data): data is OrderItem => !!data), // This ensures data is not null or undefined
-  //     map(data => ({...data, quantityCount: (data.quantityCount || 0) + 1})),
-  //     switchMap(updatedItem => this.cartService.updateItem(updatedItem))
-  //   ).subscribe({
-  //     next: (updatedItem) => {
-  //       // You can add any additional logic here, like updating a local state
-  //       this.orderQuantity = updatedItem.quantityCount;
-  //       // console.log('Item updated successfully', updatedItem);
-  //     },
-  //     error: (error) => {
-  //       console.error('Error updating item', error);
-  //       // Handle the error appropriately
-  //     }
-  //   });
-  // }
-
-  
-  // decreaseProductQuantity (productId: string) {
-  //   this.cartService.findItem(productId).pipe(
-  //     take(1),
-  //     filter((item): item is OrderItem => !!item),
-  //     map(item => ({ 
-  //       ...item, 
-  //       quantityCount: Math.max((item.quantityCount || 0) - 1, 0) 
-  //     })),
-  //     switchMap(updatedItem => this.cartService.updateItem(updatedItem))
-  //   ).subscribe({
-  //     next: (updatedItem) => {
-  //       // Optionally, you can emit this updated item or update some local state
-  //       this.orderQuantity = updatedItem.quantityCount;
-  //     },
-  //     error: (error) => {
-  //       console.error('Error decreasing item quantity:', error);
-  //     }
-  //   });
-  // }
-
-  // displayOrderQuantity (quantity: number) {
-  //   return quantity;
-  // }
-  
-
 }
